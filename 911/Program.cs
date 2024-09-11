3fd1c385f0f34ea663ad47314e1e2670aa515d8a
hunor64 <nhunor06@gmail.com>
2024-09-11 08:31:41 +0200

    1. Feladat kész

--- 911/Program.cs
+++ 911/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿if (args.Length%2 == 0 || args.Length <= 2)
+{
+    Console.WriteLine("Nem megfelelő argumentumszám!");
+}
+else
+{
+    if (Convert.ToInt32(args[(args.Length / 2) + 1]) > Convert.ToInt32(args[(args.Length / 2) - 1]))
+    {
+        Console.WriteLine($"Eredmény: {Math.Pow(Convert.ToInt32(args[args.Length / 2]), Convert.ToInt32(args[(args.Length / 2) + 1]) / Convert.ToInt32(args[(args.Length / 2) - 1]))}");
+    }
+    else 
+    {
+        Console.WriteLine($"Eredmény: {Math.Pow(Convert.ToInt32(args[args.Length / 2]), Convert.ToInt32(args[(args.Length / 2) - 1]) / Convert.ToInt32(args[(args.Length / 2) + 1]))}");
+    }
+}
