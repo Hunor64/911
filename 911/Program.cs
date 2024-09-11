3b1e8f32250cb59769307095ee065a9a21610bb2
hunor64 <nhunor06@gmail.com>
2024-09-11 08:49:22 +0200

    2. Feladat kész

--- 911/Program.cs
+++ 911/Program.cs
@@ -1,4 +1,7 @@
-﻿if (args.Length%2 == 0 || args.Length <= 2)
+﻿// 1. Feladat
+using System.Security.Cryptography.X509Certificates;
+
+if (args.Length%2 == 0 || args.Length <= 2)
 {
     Console.WriteLine("Nem megfelelő argumentumszám!");
 }
@@ -13,3 +16,34 @@ else
         Console.WriteLine($"Eredmény: {Math.Pow(Convert.ToInt32(args[args.Length / 2]), Convert.ToInt32(args[(args.Length / 2) - 1]) / Convert.ToInt32(args[(args.Length / 2) + 1]))}");
     }
 }
+
+// 2. Feladat
+List<string> szavak = File.ReadAllLines("szavak.txt").ToList();
+char[] maganhangzok = ['A', 'E', 'I', 'O', 'U'];
+int negySzotagnalTobbSzavak = 0;
+int legnagyobbSzotag = 0;
+
+foreach (var szo in szavak)
+{
+    int szotag = 0;
+
+    foreach (var betu in szo)
+    {
+        if (maganhangzok.Contains(betu))
+        {
+            szotag++;
+        }
+    }
+    if (szotag > 4)
+    {
+        negySzotagnalTobbSzavak++;
+    }
+    if (legnagyobbSzotag<szotag)
+    {
+        legnagyobbSzotag = szotag;
+    }
+}
+Console.WriteLine($"A több, mint négy szótagból álló stavak száma: {negySzotagnalTobbSzavak}");
+Console.WriteLine($"A legnagyo szótagszám: {legnagyobbSzotag}");
+
+// 3. Feladat
